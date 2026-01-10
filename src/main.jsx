faa75a9e4479584936ae1dadfdb70a9f6bdb9028
Ghulam Mustafa <mustafakamal63633@gmail.com>
2026-01-10 19:29:45 +0500

    fix some issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { NameProvider } from './Components/ContextApiLearning.jsx'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <NameProvider>
-    <App />
-  </NameProvider>
-  // </StrictMode>
+  <StrictMode>
+    <NameProvider>
+      <App />
+    </NameProvider>
+  </StrictMode>
 )

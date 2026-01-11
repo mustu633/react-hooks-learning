2eb3f2cfd28c16948ea9ddd2bd0de7ad4c4a4747
Ghulam Mustafa <mustafakamal63633@gmail.com>
2026-01-11 12:03:00 +0500

    Update App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import './App.css';
-import UseTransitionLearning from './Components/UseTransitionLearning';
 function App() {
 
   return (
     <>
-    {/* <h1>BASIC APP REACT</h1>
+    <h1>BASIC APP REACT</h1>
     <strong>This is the basic react project to Learn about Hooks in react!</strong>
-    <hr /> */}
-
-    <UseTransitionLearning/>
+    <hr />
     </>
   )
 }
